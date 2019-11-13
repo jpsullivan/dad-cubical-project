7007661a8f3c3f0de1522ae873e5ce7ca665f65d
Josh Sullivan <jaysche.sullivan@gmail.com>
2019-11-12 22:58:31 -0500

    added initial led switch program

---
+++ bt-servo.ino
@@ -0,0 +1,37 @@
+int ledPin = 13;
+int state = 0;
+int flag = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    state = Serial.read();
+    flag = 0;
+  }
+  if (state == '0')
+  {
+    digitalWrite(ledPin, LOW);
+    if (flag == 0)
+    {
+      Serial.println("LED: Off");
+      flag = 1;
+    }
+  }
+  else if (state == '1')
+  {
+    digitalWrite(ledPin, HIGH);
+    if (flag == 0)
+    {
+      Serial.println("LED: On");
+      flag = 1;
+    }
+  }
+}
